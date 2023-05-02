8f40be60ab569541563b6a7db3d64187ec59063a
Sheref Sidarous <sherefyounan@gmail.com>
2023-05-02 08:28:44 -0700

    implemented alloc functions

--- library/std/src/sys/freertos/alloc.rs
+++ library/std/src/sys/freertos/alloc.rs
@@ -1,23 +1,25 @@
 use crate::alloc::{GlobalAlloc, Layout, System};
-use crate::ptr::null_mut;
+
+extern "C" {
+    pub fn pvPortMalloc( xSize : u32 ) -> *mut u8 ;
+    pub fn vPortFree( pv : *mut u8 );
+}
+
 
 #[stable(feature = "alloc_system_type", since = "1.28.0")]
 unsafe impl GlobalAlloc for System {
     #[inline]
-    unsafe fn alloc(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        unsafe {
+            pvPortMalloc(layout.size() as u32)
+        }
     }
 
     #[inline]
-    unsafe fn alloc_zeroed(&self, _layout: Layout) -> *mut u8 {
-        null_mut()
+    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+        unsafe {
+            vPortFree(ptr);
+        }
     }
 
-    #[inline]
-    unsafe fn dealloc(&self, _ptr: *mut u8, _layout: Layout) {}
-
-    #[inline]
-    unsafe fn realloc(&self, _ptr: *mut u8, _layout: Layout, _new_size: usize) -> *mut u8 {
-        null_mut()
-    }
 }
