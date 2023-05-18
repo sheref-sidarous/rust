af33c704b0e1312cdf02a75618e3c752847297d4
Sheref Sidarous <sherefyounan@gmail.com>
2023-05-18 08:33:47 -0700

    added OS name / family in env

--- library/std/src/sys/freertos/env.rs
+++ library/std/src/sys/freertos/env.rs
@@ -1,6 +1,6 @@
 pub mod os {
-    pub const FAMILY: &str = "";
-    pub const OS: &str = "";
+    pub const FAMILY: &str = "FreeRTOS";
+    pub const OS: &str = "FreeRTOS";
     pub const DLL_PREFIX: &str = "";
     pub const DLL_SUFFIX: &str = "";
     pub const DLL_EXTENSION: &str = "";
