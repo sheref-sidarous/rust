f7d3a9c842815a12b9b57e4378912dd3e16d0612
Sheref Sidarous <sherefyounan@gmail.com>
2023-06-08 06:47:31 -0700

    making allocation aligned, this is a very inefficient way and should be revised

--- library/std/src/sys/freertos/alloc.rs
+++ library/std/src/sys/freertos/alloc.rs
@@ -10,15 +10,31 @@ extern "C" {
 unsafe impl GlobalAlloc for System {
     #[inline]
     unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
+        let size_to_alloc = layout.size() + layout.align();
+        let allocated_ptr = unsafe {
+            pvPortMalloc(size_to_alloc as u32)
+        };
+
+        // find padding and aligned_pointer
+        // make sure that at least one byte is available in padding to store
+        // the padding value (hence the .offset(1))
+        let padding = allocated_ptr.offset(1).align_offset(layout.align()) + 1;
+        let aligned_ptr = allocated_ptr.offset(padding as isize);
+
+        // store padding just before the aligned_ptr
         unsafe {
-            pvPortMalloc(layout.size() as u32)
+            *aligned_ptr.offset(-1) = padding as u8;
         }
+
+        aligned_ptr
     }
 
     #[inline]
-    unsafe fn dealloc(&self, ptr: *mut u8, _layout: Layout) {
+    unsafe fn dealloc(&self, aligned_ptr: *mut u8, _layout: Layout) {
         unsafe {
-            vPortFree(ptr);
+            let padding = *aligned_ptr.offset(-1) as isize;
+            let original_ptr = aligned_ptr.offset(-1 * padding);
+            vPortFree(original_ptr);
         }
     }
 
